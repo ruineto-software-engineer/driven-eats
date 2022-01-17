f3e641412aa136c263646c73913f5aa3afd451aa
Rui Neto <ruineto11@gmail.com>
2022-01-16 21:37:16 -0300

    README.md updated

---
+++ README.md
@@ -0,0 +1,30 @@
+# DrivenEats
+
+Selecting your order has never been so simple with DrivenEats, place your order now in our app.
+
+<!-- <img src="/assets/my-wallet-usage.gif" /> -->
+
+Try it out now at https://ruineto-dev.github.io/driven-eats/
+
+## About
+
+This web application is focused on making food ordering simple through an application, using the tecnologies:
+
+- html
+- css
+- javascript
+
+By open this website is possible see all styles and features implemented by the long in your construction.
+
+## Technologies
+The following tools were used in the construction of the project:<br>
+<p>
+  <img style='margin: 5px;' src='https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white'>
+  <img style='margin: 5px;' src='https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white'>
+  <img style='margin: 5px;' src='https://img.shields.io/badge/JavaScript-323330?style=for-the-badge&logo=javascript&logoColor=F7DF1E'>
+</p>
+
+## How to run
+
+1. Clone this repository
+2. Finally access http://localhost:3000 on your favorite browser (unless it is Internet Explorer. In this case, review your life decisions)
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,97 +1,51 @@
 function selected_first(card) {
-    //Encontrar seção que contém os pratos
     const selected = document.querySelector(".dishes");
-    //console.log(selected);
-
-    //O prato selecionado será algo dentro do menu de pratos que tenha sido selecionado
     const dish_selected = selected.querySelector(".selected");
-    //console.log(dish_selected);
-
-    //Prato selecionado (com seletor selected) deverá ter um seletor chamado "visible"
     const dish_check = selected.querySelector(".selected .visible");
-    //console.log(dish_check);
 
-    /* 
-     * Se caso existir no seletor dishes (pratos) alguém com a classe selected ou visible,
-     * devem ser removidas
-     */
     if(dish_selected !== null && dish_check !== null){
         dish_selected.classList.remove("selected");
         dish_check.classList.remove("visible");
     }
 
-    //O card é o this
     card.classList.add("selected");
     card.querySelector(".checkmark-circle").classList.add("visible");
-    //console.log(card);
 }
 
 function selected_second(card) {
-    //Encontrar seção que contém as bebidas
     const selected = document.querySelector(".drinks");
-    //console.log(selected);
-
-    //A bebida selecionada será algo dentro do menu de bebidas que tenha sido selecionada
     const drink_selected = selected.querySelector(".selected");
-    //console.log(drink_selected);
-
-    //A bebida selecionada (com seletor selected) deverá ter um seletor chamado "visible"
     const drink_check = selected.querySelector(".selected .visible");
-    //console.log(drink_check);
 
-    /* 
-     * Se caso existir no seletor drinks (bebidas) alguém com a classe selected ou visible,
-     * devem ser removidas
-     */
     if(drink_selected !== null && drink_check !== null){
         drink_selected.classList.remove("selected");
         drink_check.classList.remove("visible");
     }
     
-    //O card é o this
     card.classList.add("selected");
     card.querySelector(".checkmark-circle").classList.add("visible");
-    //console.log(card);
 }
 
 function selected_third(card) {
-    //Encontrar seção que contém as bebidas
     const selected = document.querySelector(".desserts");
-    //console.log(selected);
-
-    //A sobremesa selecionada será algo dentro do menu de sobremesas que tenha sido selecionada
     const dessert_selected = selected.querySelector(".selected");
-
-    //A sobremesa selecionada (com seletor selected) deverá ter um seletor chamado "visible"
     const dessert_check = selected.querySelector(".selected .visible");
 
-    /* 
-     * Se caso existir no seletor desserts (sobremesas) alguém com a classe selected ou visible,
-     * devem ser removidas
-     */
     if(dessert_selected !== null && dessert_check !== null){
         dessert_selected.classList.remove("selected");
         dessert_check.classList.remove("visible");
     }
     
-    //O card é o this
     card.classList.add("selected");
     card.querySelector(".checkmark-circle").classList.add("visible");
-    //console.log(card); 
 }
 
 function enabled_button() {
     const selections = document.querySelectorAll(".selected");
-    //selections[0].innerText[0];
-    //console.log(selections);
-
     const selections_count = selections.length;
     
     if(selections_count === 3){
-        //console.log(selections_count);
         const enabled_button = document.querySelector(".btn-bottom-bar");
-        
-        //console.log(enabled_button);
 
         enabled_button.classList.add("btn-bottom-bar-custom");
         enabled_button.innerHTML = "Fechar pedido";
@@ -111,32 +65,20 @@ function send_request() {
     let tile_second_product = title_product[1].innerText;
     let tile_third_product = title_product[2].innerText;
 
-    //console.log(tile_first_product);
-    //console.log(tile_second_product);
-    //console.log(tile_third_product);
-
     const enabled_send_request = document.querySelector(".btn-bottom-bar-custom");
-    //console.log(enabled_send_request);
-
     const name = prompt("Informe seu nome e sobrenome:");
     const address = prompt("Informe seu endereço:");
 
     if(enabled_send_request !== null && name !== null && address !== null){
-        //alert("Enviar pedido!");
-
         let custom_message = "Olá, gostaria de fazer o pedido:" +"\n"+ "- Prato: " + tile_first_product +"\n"+ "- Bebida: " + tile_second_product +"\n"+ "- Sobremesa: " + tile_third_product +"\n"+ "Total: R$ " + total_price() +"\n\n"+ "Nome: " + name +"\n"+ "Endereço: " + address;
-        //console.log(custom_message);
 
         open ("https://wa.me/" + phone_number + "?text=" + encodeURIComponent(custom_message));
-        //window.location.href = "https://wa.me/5588997653298?text=Eu%20tenho%20interesse%20no%20seu%20carro%20à%20venda";
     }  
 }
 
 function send_request_auxiliary() {
     const confirm_order = document.querySelector(".confirm-order");
     const selections = document.querySelectorAll(".selected");
-    //selections[0].innerText[0];
-    //console.log(selections);
 
     const selections_count = selections.length;
     
@@ -170,10 +112,6 @@ function replace_order_items() {
         tile_third_product = title_product[2].innerText;
     }
 
-    // console.log(tile_first_product);
-    // console.log(tile_second_product);
-    // console.log(tile_third_product);
-
     first_product = confirm_order_items.querySelector(".first-product-item");
     first_product.innerHTML = tile_first_product;
 
@@ -182,8 +120,6 @@ function replace_order_items() {
 
     third_product = confirm_order_items.querySelector(".third-product-item");
     third_product.innerHTML = tile_third_product;
-
-    //console.log(first_product);
 }
 
 function replace_order_values() {
@@ -228,10 +164,6 @@ function replace_order_values() {
 
     let total_custom = "R$ " + total.replace(".", ",");
 
-    // console.log(tile_first_product);
-    // console.log(tile_second_product);
-    // console.log(tile_third_product);
-
     first_product = confirm_order_items.querySelector(".first-product-item-value");
     first_product.innerHTML = price_first_product_custom;
 
@@ -243,8 +175,6 @@ function replace_order_values() {
 
     total_products = confirm_order_items.querySelector(".total-product-items-value");
     total_products.innerHTML = total_custom;
-
-    //console.log(first_product);   
 }
 
 function total_price() {
@@ -267,12 +197,6 @@ function total_price() {
     }
   
     let total = (price_first_product + price_second_product + price_third_product).toFixed(2);
-
-    //console.log(price_first_product);
-    //console.log(price_second_product);
-    //console.log(price_third_product);
-
-    //console.log(total);
 
     return total;
 }
